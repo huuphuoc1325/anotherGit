55bb8d5b1bd780de7527cd6604bcf5e747f3ac37
Huu Phuoc <gamemakerrpg1325@gmail.com>
2021-03-07 15:07:59 +0700

    init class student

---
+++ student.h
@@ -0,0 +1,19 @@
+#include <string.h>
+
+class student
+{
+private:
+    int id;
+    std::string name;
+public:
+    student(/* args */);
+    ~student();
+};
+
+student::student(/* args */)
+{
+}
+
+student::~student()
+{
+}
